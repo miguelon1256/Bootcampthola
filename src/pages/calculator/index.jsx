c1a8055d93ea61fc72e505a9bc6fb3a359ca7eb5
miguelon1256 <mtholac@gmail.com>
2022-11-08 22:56:57 -0400

    Suna, resta, division y multiplicacion

--- src/pages/calculator/index.jsx
+++ src/pages/calculator/index.jsx
@@ -1,1 +1,49 @@
-export const Calculator = () => <h1>calculator</h1>
+import { useEffect, useState } from "react";
+
+export function Calculator(){
+    
+    const [number, setNumber] = useState({one: 0, two: 0, three: 0})
+    const [suma, setSuma] = useState()
+    const [resta, setResta] = useState()
+    const [multiplicacion, setMultiplicacion] = useState()
+    const [division, setDivision] = useState()
+
+  
+    useEffect(()=>{
+      const { one, two, three } = number
+      setSuma(Number(one) + Number(two) + Number(three))
+    },[number])
+
+    useEffect(()=>{
+        const { one, two, three } = number
+        setResta(Number(one) - Number(two) - Number(three))
+      },[number])
+
+      useEffect(()=>{
+        const { one, two, three } = number
+        setMultiplicacion(Number(one) * Number(two) * Number(three))
+      },[number])
+
+      useEffect(()=>{
+        const { one, two, three } = number
+        setDivision(Number(one) / Number(two))
+      },[number])
+  
+    const handleInput = (event) => {
+      const { name, value } = event.target
+      setNumber({...number, [name]: value})
+    }
+  
+    return (
+      <div className="App">
+        <input className="input" onChange={handleInput} name="one" value={number.one} placeholder="Ingesa un numero (1)"/>
+        <input className="input" onChange={handleInput} name="two" value={number.two} placeholder="Ingesa un numero (2)"/>
+        <input className="input" onChange={handleInput} name="three" value={number.three} placeholder="Ingesa un numero (3)"/>
+        <input value={suma}/>
+        <input value={resta}/>
+        <input value={multiplicacion}/>
+        <input value={division}/>
+
+      </div>
+    );
+};
